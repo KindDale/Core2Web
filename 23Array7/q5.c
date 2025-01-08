45cff0265ade8ee5b934301beb4ded599e43ceb9
Deepankar Rokade <dsrcubes123@gmail.com>
2025-01-08 14:43:05 +0530

    Add files via upload

---
+++ 23Array7/q5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void main () {
+	int size;
+	printf("Enter the size of the array = ");
+	scanf("%d",&size);
+
+	int arr[size], freq[size];
+
+	printf("Enter the array elements\n");
+
+	for (int i = 0; i < size; i++){
+		freq[i] = -1;
+		scanf("%d",&arr[i]);
+	}
+
+	for (int i = 0; i < size; i++){
+		int count = 1;
+		for (int j = i + 1; j < size; j++){
+			if (arr[i] == arr[j]){
+				count++;
+				freq[j] = 0;
+			}
+
+		}
+		if (freq[i] != 0){
+			freq[i] = count;
+		}
+	}
+
+	
+	for (int i = 0; i < size; i++){
+		if (freq[i] > size/2)
+			printf("Majority element = %d\n",arr[i]);
+	}
+}
